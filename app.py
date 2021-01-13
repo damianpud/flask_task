a48a5354e075c5ca04137d839a73deae1b71a855
Damian <damianpud@gmail.com>
2021-01-13 18:09:40 +0100

    start app

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+
+@app.route('/')
+def hello_flask():
+    return render_template(
+        'hello.html'
+    )
+
+
+if __name__ == '__main__':
+    app.run()
+
