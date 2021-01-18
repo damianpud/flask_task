37b4e22225f40f23b662e28b27773f1ccc0cd11b
Damian <damianpud@gmail.com>
2021-01-18 12:27:17 +0100

    migrations

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_migrate import Migrate
 
 from bookstore.views import main_blueprint
 from bookstore.models import db
@@ -9,6 +10,7 @@ app.register_blueprint(main_blueprint)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
+Migrate(app, db)
 Bootstrap(app)
 
 
