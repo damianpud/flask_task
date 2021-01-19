19644669c2f588b8ee47b1beae9ab8e007d5353c
Damian <damianpud@gmail.com>
2021-01-19 14:52:58 +0100

    categoreis view

--- app.py
+++ app.py
@@ -15,5 +15,5 @@ Bootstrap(app)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
--- bookstore/views.py
+++ bookstore/views.py
@@ -1,8 +1,20 @@
 from flask import Blueprint, render_template
 
+from bookstore import models
+
 main_blueprint = Blueprint('main', __name__)
 
 
 @main_blueprint.route('/')
 def hello_flask():
     return render_template('hello.html')
+
+
+@main_blueprint.route('/categories')
+def categories():
+    return render_template('categories.html', categories=models.Category.query)
+
+
+@main_blueprint.route('/books')
+def books():
+    return render_template('books.html', books=models.Book.query)
