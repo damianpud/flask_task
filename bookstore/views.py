3401d34d7532534234cee18a023ecea6c9c50023
Damian <damianpud@gmail.com>
2021-01-31 20:41:25 +0100

    login manager and new login/logout

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash, session
 from flask_wtf import FlaskForm
+from flask_login import LoginManager, login_required, login_user, logout_user, current_user
 
 from bookstore import models
 from bookstore import forms
@@ -7,6 +8,13 @@ from bookstore import forms
 from werkzeug.security import generate_password_hash, check_password_hash
 
 main_blueprint = Blueprint('main', __name__)
+login_manager = LoginManager()
+login_manager.login_view = 'main.login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return models.User.query.get(user_id)
 
 
 @main_blueprint.route('/')
@@ -102,17 +110,21 @@ def register():
 
 @main_blueprint.route('/login', methods=['GET', 'POST'])
 def login():
-    form = forms.LoginForm(request.form)
+    form = forms.LoginForm()
     if not form.validate_on_submit():
         return render_template('login.html', form=form)
     user = models.User.query.filter_by(email=form.email.data).first()
     if user:
         if check_password_hash(user.password, form.password.data):
-            flash('You have successfully logged in.', "success")
-            session['logged_in'] = True
-            session['email'] = user.email
-            session['username'] = user.username
-            return redirect(url_for('main.books'))
+            login_user(user, remember=True)
+            next_page = request.args.get('next')
+            return redirect(next_page or url_for('main.books'))
         else:
-            flash('Username or Password Incorrect', "Danger")
-            return redirect(url_for('/login'))
+            return redirect(url_for('main.login'))
+
+
+@main_blueprint.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.books'))
