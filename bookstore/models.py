866cfce9e59837e50f503e231dc47a045c747a6f
Damian <damianpud@gmail.com>
2021-02-09 10:51:47 +0100

    Role model with association table to Users

--- bookstore/models.py
+++ bookstore/models.py
@@ -2,10 +2,9 @@ from datetime import datetime
 
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
-from sqlalchemy.sql.functions import concat
 
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_security import RoleMixin, UserMixin
 
 db = SQLAlchemy()
 
@@ -39,13 +38,32 @@ class Author(db.Model):
     author_books = relationship('Book', back_populates='author')
 
 
+roles_users = db.Table(
+    'roles_users',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('role_id', db.Integer, db.ForeignKey('role.id'))
+)
+
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(length=128), nullable=False)
     email = db.Column(db.String(length=128), nullable=False, unique=True)
     password = db.Column(db.String(length=256), unique=True)
     created = db.Column(db.DateTime, default=datetime.utcnow)
+    active = db.Column(db.Boolean, default=True)
     order = relationship('Order', back_populates='user')
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+
+class Role(RoleMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(length=80), unique=True)
+    description = db.Column(db.String(length=255))
+
+    def __str__(self):
+        return self.name
 
 
 class Order(db.Model):
