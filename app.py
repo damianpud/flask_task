ee16aa685fd91366152f93501a1be797436d9b6b
Damian <damianpud@gmail.com>
2021-02-12 13:18:47 +0100

    install flask uploads

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_migrate import Migrate
+from flask_uploads import configure_uploads
 import click
 from click_params import EMAIL
 
-from bookstore.views import main_blueprint, login_manager, admin, create_superuser
+from bookstore.views import main_blueprint, login_manager, admin, create_superuser, images
 from bookstore.models import db
 
 app = Flask(__name__)
@@ -12,11 +13,13 @@ app.register_blueprint(main_blueprint)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'Fnioz1Cnl2grWSA2MLEbCrBuJjJK0ELB'
+app.config['UPLOADED_IMAGES_DEST'] = 'static/media'
 admin.init_app(app)
 db.init_app(app)
 Migrate(app, db)
 Bootstrap(app)
 login_manager.init_app(app)
+configure_uploads(app, images)
 
 _CREATE_SUPERUSER_HELP = (
     'Create superuser account.'
