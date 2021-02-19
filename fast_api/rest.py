1185958f39c106acccbed114f0e5e30f5e70880d
Damian <damianpud@gmail.com>
2021-02-19 13:15:19 +0100

    added new rest views

--- fast_api/rest.py
+++ fast_api/rest.py
@@ -21,19 +21,48 @@ def index():
 
 
 @routeapi.get('/author/all')
-def show_all(db: Session = Depends(get_db)):
+def show_all_authors(db: Session = Depends(get_db)):
     author = db.query(models.Author).all()
     return author
 
 
 @routeapi.get('/author/{author_id}', status_code=200)
-def show(author_id, db: Session = Depends(get_db)):
+def show_author(author_id, db: Session = Depends(get_db)):
     author = db.query(models.Author).filter(models.Author.id == author_id).first()
     if not author:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'Author with the id {author_id} in not available')
     return author
 
+
+@routeapi.get('/category/all')
+def show_all_categories(db: Session = Depends(get_db)):
+    categories = db.query(models.Category).all()
+    return categories
+
+
+@routeapi.get('/category/{category_id}', status_code=200)
+def show_category(category_id, db: Session = Depends(get_db)):
+    category = db.query(models.Category).filter(models.Category.id == category_id).first()
+    if not category:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Author with the id {category_id} in not available')
+    return category
+
+
+@routeapi.get('/book/all')
+def show_all_books(db: Session = Depends(get_db)):
+    books = db.query(models.Book).all()
+    return books
+
+
+@routeapi.get('/book/{book_id}', status_code=200)
+def show_book(book_id, db: Session = Depends(get_db)):
+    book = db.query(models.Book).filter(models.Book.id == book_id).first()
+    if not book:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Author with the id {book_id} in not available')
+    return book
 
 
 
